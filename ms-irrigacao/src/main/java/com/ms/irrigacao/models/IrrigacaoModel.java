835138deb22b099eefec57a7795b1adc9cc9a4e5
Marlon Santos <30815507+MarlonSS@users.noreply.github.com>
2021-10-31 20:38:07 -0300

    Add files via upload

--- ms-irrigacao/src/main/java/com/ms/irrigacao/models/IrrigacaoModel.java
+++ ms-irrigacao/src/main/java/com/ms/irrigacao/models/IrrigacaoModel.java
@@ -3,17 +3,25 @@ package com.ms.irrigacao.models;
 import com.ms.irrigacao.enums.StatusIrrigacao;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "irrigacao")
 public class IrrigacaoModel {
-    private static final long serialVersionUID = 1L;
-
+   // private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    @Column(name="vazao")
     private String vazaoAgua;
-
+    @Column(name="tamanhoarea")
     private String tamanhoArea;
-
+    @Column(name="tipojato")
     private String tipoJato;
-
+    @Column(name="tempoirrigacao")
     private String tempoIrrigacao;
-
+    @Column(name = "")
     private StatusIrrigacao statusIrrigacao;
 }
