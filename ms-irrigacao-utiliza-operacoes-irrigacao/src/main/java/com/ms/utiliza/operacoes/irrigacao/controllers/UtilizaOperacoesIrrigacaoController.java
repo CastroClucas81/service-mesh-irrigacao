eaa0a63611319e22aeda5c4144038470188ae8db
Lucas Cardoso de Castro <sky.lucas2013@gmail.com>
2021-11-10 20:03:25 -0300

    second commit

--- ms-irrigacao-utiliza-operacoes-irrigacao/src/main/java/com/ms/utiliza/operacoes/irrigacao/controllers/UtilizaOperacoesIrrigacaoController.java
+++ ms-irrigacao-utiliza-operacoes-irrigacao/src/main/java/com/ms/utiliza/operacoes/irrigacao/controllers/UtilizaOperacoesIrrigacaoController.java
@@ -3,6 +3,7 @@ package com.ms.utiliza.operacoes.irrigacao.controllers;
 import com.ms.utiliza.operacoes.irrigacao.dtos.IrrigacaoDto;
 import com.ms.utiliza.operacoes.irrigacao.models.IrrigacaoModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,9 @@ import javax.validation.Valid;
 
 @RestController
 public class UtilizaOperacoesIrrigacaoController {
-    private static final String ROTA_BASE = "http://localhost:8080/";
+
+    @Value("${rota.base}")
+    private String rotaBase;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -23,14 +26,14 @@ public class UtilizaOperacoesIrrigacaoController {
     @PostMapping("/usar-irrigacao/ativar")
     public ResponseEntity<IrrigacaoModel> usarIrrigacaoAtivar(@RequestBody @Valid IrrigacaoDto irrigacaoDto) {
 
-        IrrigacaoModel irrigacao = restTemplate.postForObject(ROTA_BASE + "ativar-irrigacao", irrigacaoDto, IrrigacaoModel.class);
+        IrrigacaoModel irrigacao = restTemplate.postForObject(rotaBase + "ativar-irrigacao", irrigacaoDto, IrrigacaoModel.class);
         return new ResponseEntity<>(irrigacao, HttpStatus.CREATED);
     }
 
     @PostMapping("/usar-irrigacao/desativar")
     public ResponseEntity<IrrigacaoModel> usarIrrigacaoDesativar(@RequestBody @Valid IrrigacaoDto irrigacaoDto) {
 
-        IrrigacaoModel irrigacao = restTemplate.postForObject(ROTA_BASE + "desativar-irrigacao", irrigacaoDto, IrrigacaoModel.class);
+        IrrigacaoModel irrigacao = restTemplate.postForObject(rotaBase + "desativar-irrigacao", irrigacaoDto, IrrigacaoModel.class);
         return new ResponseEntity<>(irrigacao, HttpStatus.CREATED);
     }
 
